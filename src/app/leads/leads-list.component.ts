a05666800559f286d85d30248e6e2d53aabcb0f6
eslamahmedabozeid <60574969+eslamahmedabozeid@users.noreply.github.com>
2024-05-12 13:02:08 +0300

    angular

--- src/app/leads/leads-list.component.ts
+++ src/app/leads/leads-list.component.ts
@@ -1,3 +1,4 @@
+import { map } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Lead } from './leads.model';
@@ -12,12 +13,40 @@ import { LeadsService } from './leads.service';
 
 export class LeadsListComponent implements OnInit {
   leads$!: Observable<Lead[]>;
-  duplicates:any
+  leads:any
   constructor(private leadsService: LeadsService) { }
 
   ngOnInit(): void {
       this.leads$ = this.leadsService.getLeads();
+      this.loadLeads()
   }
+
+  loadLeads(): void {
+    this.leadsService.getLeads().subscribe((leads) => {
+      this.leads = leads;
+      console.log(this.leads);
+  
+      // Use Set to store the lead_ids encountered so far
+      const leadIdsSet = new Set<string>();
+  
+      // Check for duplicate lead_ids
+      let hasDuplicate = false;
+      this.leads.forEach((item: any) => {
+        if (leadIdsSet.has(item.lead_id)) {
+          hasDuplicate = true;
+          return; // Exit the loop if a duplicate is found
+        }
+        leadIdsSet.add(item.lead_id);
+      });
+  
+      if (hasDuplicate) {
+        console.log('Duplicate lead_ids found.');
+      } else {
+        console.log('No duplicate lead_ids found.');
+      }
+    });
+  }
+  
 
   duplicatesMap: { [key: string]: any[] } = {};
   loadPotentialDuplicates(leadId: string): void {
